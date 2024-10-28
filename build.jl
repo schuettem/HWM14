f9a7a4c158a52a4e595b0a2f2e57141c04d56b5a
Miklas Schütte <schuettem@irs.uni-stuttgart.de>
2024-10-28 14:42:08 +0100

    comment

--- build.jl
+++ build.jl
@@ -5,7 +5,7 @@ src_dir = joinpath(@__DIR__, "src/", "hwm14_fortran")
 lib_path = joinpath(src_dir, "libhwm14.so")
 fortran_source = joinpath(src_dir, "hwm14.f90")
 
-# Checl if the library exists
+# Check if the library exists
 if !isfile(lib_path)
     println("Compiling Fortran code ...")
 
