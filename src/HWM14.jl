27c37f7c724c5a5d71d52c0b9f99600983f03c2e
Miklas Schütte <schuettem@irs.uni-stuttgart.de>
2024-10-28 14:41:14 +0100

    modified function inputs and time conversion

--- src/HWM14.jl
+++ src/HWM14.jl
@@ -6,7 +6,7 @@ using Dates
 const libhwm14_path = joinpath(@__DIR__, "hwm14_fortran", "libhwm14.so")
 
 # Get total winds:
-function hwm14(date::DateTime, alt::Float64, glat::Float64, glon::Float64, stl::Float64, f107a::Float64, f107::Float64, ap::Array{Float64,1})
+function hwm14(iyd::Int64, sec::Float64, alt::Float64, glat::Float64, glon::Float64, stl::Float64, f107a::Float64, f107::Float64, ap::Array{Float64,1})
     # date: Date and time in UTC
     # alt: Altitude in km
     # glat: Geodedic latitude in degrees
@@ -18,16 +18,6 @@ function hwm14(date::DateTime, alt::Float64, glat::Float64, glon::Float64, stl::
     # Output: w[1]: meridional wind (m/s + northward), w[2]: zonal wind (m/s + eastward)
     libhwm14 = dlopen(libhwm14_path)
     hwm14_sym = Libdl.dlsym(libhwm14, :hwm14_)
-
-    year = Dates.year(date) % 100 # Get the year as yy
-    doy = Dates.dayofyear(date) # Get the day of the year
-    iyd = year * 1000 + doy # Convert the date to year and day
-
-    # Calculate the time in seconds since midnight UT
-    hour = Dates.hour(date)
-    min = Dates.minute(date)
-    sec = Dates.second(date)
-    sec = sec + min*60.0 + hour*3600.0
 
     ap = [Float32(ap[1]), Float32(ap[2])]
     w = Float32[0.0, 0.0] # Initialize the output array
@@ -41,7 +31,7 @@ end
 # Get quiet time winds: HWM14 with a negative value for ap[2]
 
 # Get disturbance winds in geographic coordinates:
-function dwm07(date::DateTime, alt::Float32, glat::Float32, glon::Float32, ap::Array{Float32,1})
+function dwm07(iyd::Int64, sec::Float64, alt::Float64, glat::Float64, glon::Float64, ap::Array{Float64,1})
     # date: Date and time in UTC
     # alt: Altitude in km
     # glat: Geodedic latitude in degrees
@@ -51,16 +41,7 @@ function dwm07(date::DateTime, alt::Float32, glat::Float32, glon::Float32, ap::A
     libhwm14 = dlopen(libhwm14_path)
     dwm07_sym = Libdl.dlsym(libhwm14, :dwm07_)
 
-    year = Dates.year(date) % 100 # Get the year as yy
-    doy = Dates.dayofyear(date) # Get the day of the year
-    iyd = year * 1000 + doy # Convert the date to year and day
-
-    # Calculate the time in seconds since midnight UT
-    hour = Dates.hour(date)
-    min = Dates.minute(date)
-    sec = Dates.second(date)
-    sec = sec + min*60.0 + hour*3600.0
-
+    ap = [Float32(ap[1]), Float32(ap[2])]
     dw = Float32[0.0, 0.0] # Initialize the output array
     ccall(dwm07_sym, Cvoid,
           (Ref{Int32}, Ref{Float32}, Ref{Float32}, Ref{Float32}, Ref{Float32}, Ref{Float32}, Ref{Float32}),
@@ -69,7 +50,7 @@ function dwm07(date::DateTime, alt::Float32, glat::Float32, glon::Float32, ap::A
 end
 
 # Get disturbance winds in magnetic coordinates:
-function dwm07b(mlt::Float32, mlat::Float32, kp::Float32)
+function dwm07b(mlt::Float64, mlat::Float64, kp::Float64)
     # mlt: Magnetic local time in hours
     # mlat: Magnetic latitude in degrees
     # kp: current 3hr KP index
@@ -77,12 +58,51 @@ function dwm07b(mlt::Float32, mlat::Float32, kp::Float32)
     libhwm14 = dlopen(libhwm14_path)
     dwm07b_sym = Libdl.dlsym(libhwm14, :dwm07b_)
 
-    mmpwind = Float32(0.0) # Initialize the Output
-    mzpwind = Float32(0.0) # Initialize the Output
+    mmpwind = Ref{Float32}(1.0) # Initialize the Output
+    mzpwind = Ref{Float32}(1.0) # Initialize the Output
     ccall(dwm07b_sym, Cvoid,
           (Ref{Float32}, Ref{Float32}, Ref{Float32}, Ref{Float32}, Ref{Float32}),
           mlt, mlat, kp, mmpwind, mzpwind)
-    return [mmpwind, mzpwind]
+    return [mmpwind[], mzpwind[]]
+end
+
+function iydsec2datetime(iyd::Int64, sec::Float64)
+    # Calculate the year
+    yy = div(iyd, 1000)
+    yyyy_now = Dates.year(now())
+    if yy > yyyy_now % 100 # Check if yy is in the 20th or 21st century
+        yyyy = 1900 + yy
+    else
+        yyyy = 2000 + yy
+    end
+
+    # Calculate the month and day
+    doy = iyd % 1000
+    yyyy_mm_dd = Date(yyyy, 1, 1) + Day(doy - 1)
+
+    # Calculate the time in UTC
+    hour = div(sec, 3600)
+    sec = sec - hour*3600
+    minute = div(sec, 60)
+    sec = sec - minute*60
+
+    # Create the date and time as a DateTime object
+    date = DateTime(yyyy_mm_dd) + Hour(hour) + Minute(minute) + Second(sec)
+    return date
+end
+
+function datetime2iydsec(date::DateTime)
+    # Calculate the year and day of the year
+    yyyy = Dates.year(date)
+    doy = Dates.dayofyear(date)
+    iyd = (yyyy % 100) * 1000 + doy
+
+    # Calculate the time in seconds since midnight UT
+    hour = Dates.hour(date)
+    minute = Dates.minute(date)
+    sec = Dates.second(date)
+    sec = sec + minute*60.0 + hour*3600.0
+    return iyd, sec
 end
 
 end # module HWM14
